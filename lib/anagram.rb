0788d3a9ccc4f3944ff57a19d2c6fea673f76b5d
Rakitzis45 <michael.rakitzis@gmail.com>
2020-10-07 14:34:06 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word                             #sets reader/writer
+    def initialize(word)
+        @word = word
+    end
+    def match(array)                                   #method that is given an argument thats an array
+        array.select do |i|                            #.select to go through all the words in the array
+            i.split("").sort == @word.split("").sort #breaks down words into strings and sorts them
+        end                                          # to see if they have the same order of letters at the end
+    end
+end
